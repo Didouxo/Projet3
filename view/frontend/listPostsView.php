ccf4c4b3a4c9fa692c2b1f9723840ca770064b75
Didou <d.ind@hotmail.fr>
2018-02-27 17:41:52 +0100

    Mise en page avec bootstrap de listPostsView et du template et 2/3 petites choses sur les autres pages

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -1,30 +1,24 @@
 <?php $title = 'Mon blog'; ?>
 
 <?php ob_start(); ?>
-<h1>Mon super blog !</h1>
-<p>Derniers billets du blog :</p>
 
-
-<?php
-while ($data = $posts->fetch())
-{
-?>
-    <div class="news">
-        <h3>
-            <?= htmlspecialchars($data['title']) ?>
-            <em>le <?= $data['creation_date_fr'] ?></em>
-        </h3>
-
-        <p>
-            <?= nl2br(htmlspecialchars($data['content'])) ?>
-            <br />
-            <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
-        </p>
+  <div class="container">
+    <div class="row">
+    <?php while ($data = $posts->fetch()) { ?>
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title"><?= htmlspecialchars($data['title']) ?></h5>
+          <p class="text-muted"><?= $data['creation_date_fr'] ?></p>
+          <p class="card-text"><?= substr(nl2br(htmlspecialchars($data['content'])), 0, 100); ?>...</p>
+          <a href="index.php?action=post&amp;id=<?= $data['id'] ?>" class="btn btn-primary">Lire l'article</a>
+        </div>
+      </div>
     </div>
-<?php
-}
-$posts->closeCursor();
-?>
+    <?php } ?>
+  </div>
+</div>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('view/frontend/template.php'); ?>
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -4,9 +4,38 @@
         <meta charset="utf-8" />
         <title><?= $title ?></title>
         <link href="public/css/style.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     </head>
+
+
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#2196f3;">
+        <div class="container">
+  <a class="navbar-brand" href="#"><span class="ion-leaf"> </span>ALASKA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="form-inline my-2 my-lg-0">
+    <a href="index.php?action=connect" class="btn btn-outline-light">Connexion</a>
+  </div>
+  </div>
+</nav>
+    </header>
+
 
     <body>
         <?= $content ?>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </body>
+    <footer class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="ligne col-lg-6"></div>
+      </div>
+      <div class="d-flex justify-content-center">
+        <p>© ALASKA 2018</p>
+      </div>
+    </footer>
 </html>
